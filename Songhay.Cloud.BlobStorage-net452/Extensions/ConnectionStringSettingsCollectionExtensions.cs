07e3a1b25b0151b29b852e05eb4082c51add1f4c
Bryan Wilhite <rasx@songhaysystem.com>
2017-11-05 15:22:10 -0800

    :gem: :rocket: NuGet release (again): ConnectionStringSettingsCollectionExtensions added for net452

---
+++ Songhay.Cloud.BlobStorage-net452/Extensions/ConnectionStringSettingsCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Configuration;
+
+namespace Songhay.Cloud.BlobStorage.Extensions
+{
+    /// <summary>
+    /// Extensions of <see cref="ConnectionStringsCollection"/>
+    /// </summary>
+    public static class ConnectionStringSettingsCollectionExtensions
+    {
+        /// <summary>
+        /// Gets the cloud storage account.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">The expected collection is not here.</exception>
+        public static CloudStorageAccount GetCloudStorageAccount(this ConnectionStringSettingsCollection collection, string key)
+        {
+            if (collection == null) throw new ArgumentNullException("The expected collection is not here.");
+
+            var cnn = collection[key];
+            if (cnn == null) throw new ArgumentNullException("The expected settings are not here.");
+
+            var account = CloudStorageAccount.Parse(cnn.ConnectionString);
+            return account;
+        }
+    }
+}
